2b7394ee80b1696210cd314514866b28706058a9
Mason Hall <masonhall@mcpsad.net>
2022-06-06 06:45:52 -0400

    Initialized Repository

---
+++ DefaultAppPreset.ps1
@@ -0,0 +1,11 @@
+﻿$networkPath = $($args[0])
+
+$localPath = $($args[1])
+
+$defaultPath = $($args[2])
+
+Copy-Item $networkPath $localPath
+
+Copy-Item $defaultPath $env:TEMP
+
+Start-Process -FilePath $localPath -Verb RunAs
