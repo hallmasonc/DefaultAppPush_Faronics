12fb770cd11a081ea606716404440fc3d27c11e8
Mason Hall <masonhall@mcpsad.net>
2022-06-06 11:18:18 -0400

    Added Comments

--- DefaultAppPreset.ps1
+++ DefaultAppPreset.ps1
@@ -1,4 +1,14 @@
-﻿$netScriptPath = $($args[0])
+﻿<#
+    This script is used in Faronics Deploy to copy Batch
+    and PowerShell Scripts to a machine from a share drive.
+    Default Path is any file that can be copied to TEMP folder,
+    for example: If you have an .xml file that the copied
+    script relies upon.
+
+    Author: Mason Hall and Jeff Ritchie
+    Date: 6/6/2022
+#>
+$netScriptPath = $($args[0])
 
 $netBatchPath = $($args[1])
 
